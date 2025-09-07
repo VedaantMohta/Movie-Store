e375c8d55513da4bffd6616261bdefa320fbe7a6
Vedaant Mohta <vedaantmohta@gmail.com>
2025-09-07 18:13:06 -0400

    add cart and orders

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required
 def logout(request):
@@ -46,3 +47,10 @@ def signup(request):
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
                 {'template_data': template_data})
+        
+@login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html', {'template_data': template_data})
